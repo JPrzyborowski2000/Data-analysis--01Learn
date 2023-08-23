96a17cdc5bbd3228195fe7c8dac295faa4ceb3b5
JPrzyborowski2000 <102661173+JPrzyborowski2000@users.noreply.github.com>
2023-08-23 08:18:43 +0200

    Update 01TaskPandas.py

--- 01TaskPandas.py
+++ 01TaskPandas.py
@@ -2,8 +2,10 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import pandasgui
+import tables
 
-# Generate Test Date
+# Generate Test Data01
+'''
 num_days = 20
 temperature = np.random.uniform(20, 28, size=(num_days, 1))
 pressure = np.random.uniform(990, 1010, size=(num_days, 1))
@@ -12,7 +14,6 @@ random_data = np.hstack((temperature, pressure, rain))
 df_weather = pd.DataFrame(index=pd.date_range("20200501", periods=num_days, freq="1D"),
                          data=random_data, columns=["Temperature", "Pressure", "Rain"])
 
-'''
 #Print Data
 print(df_weather)
 
@@ -33,8 +34,60 @@ plt.show()
 pandasgui.show(df_weather, settins ={'block':True})
 '''
 
-# New Test Data02
-
+# Generate Test Data02
+'''
 vals = np.random.randn(6,4)
 df = pd.DataFrame(vals, index = [0.0, 0.2, 0.3, 0.7, 1.0, 1.3], columns = ["A", "B", "C", "D"])
-print(df)
+
+#print(df)
+#print(df.index)
+#print(df.columns)
+
+list_of_columns = list(df.columns)
+
+for c in list_of_columns:
+    print(c)
+
+df.values #--> data to table
+
+#Get data from col or index
+df['B']
+df[['C','D']]
+df[0.2:1.0]
+#df.loc by name df.iloc -> like numpy array
+#print(df.iloc[0:3, df.columns.get_loc('C')]) # mix index of row and name of col
+#print(df.loc[(df["A"] > -0.75) & (df["B"] < 0.25), :])
+'''
+
+# Generate Test Data03
+
+alpha = np.array([0, np.pi/4, np.pi/2, np.pi*3/4, np.pi])
+
+trig = pd.DataFrame({"sinus": np.round(np.sin(alpha), 10),
+                     "cosinus" : np.round(np.cos(alpha), 10),
+                     "x^2" : alpha**2,
+                     "random" : np.random.randn(len(alpha))}, index=alpha)
+
+#trig.loc[0:4, "random"] = 0
+trig.loc[trig["cosinus"] >= 0, "random"] = np.array([1, 3, 5])
+#trig["New column"] = -1 #->New Col
+#trig.loc[1337, :] = -1 #->New Row
+
+#trig.set_index(trig["sinus"], inplace=True) -> to set new index from sinus val, inplace do not create new obj
+
+
+#rename_dict = {"sinus": "sin", "cosinus": "cos"}
+#trig.rename(columns=rename_dict, inplace=True) #-> change col name 
+
+#trig.sort_values("cos", axis=0, inplace=True, ascending=False)  # sort_by_row in cosine col, same_thing, decrease
+
+#Save and Read file
+
+trig.to_csv("trig.csv")
+trig_from_csv = pd.DataFrame(pd.read_csv("trig.csv", index_col=0)) #index_col -> col like index
+print(trig)
+print(trig_from_csv)
+
+#HDF5 format is faster and more complex
+trig.to_hdf("trig.hdf5", "data", format="fixed", mode="w", complevel=5)  
+trig_from_hdf = pd.DataFrame(pd.read_hdf("trig.hdf5"))
